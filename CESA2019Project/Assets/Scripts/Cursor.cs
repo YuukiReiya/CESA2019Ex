2b24cdb5b814cf17404515d93b74889b9a21963b
tukisiro <tksrtuy@gmail.com>
2019-02-26 15:29:18 +0900

    [野沢]　カーソルのスクリプト作成途中

---
+++ CESA2019Project/Assets/Scripts/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cursor : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform trans = this.transform;
+        Vector3 localPos = trans.localPosition;
+        if (Input.GetAxis("GamePad0_LJoystick_Y") > 0.5f)
+        {
+            localPos.y = 1.0f;
+        }
+    }
+    }
+
