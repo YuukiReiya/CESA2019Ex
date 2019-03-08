9599207b9393aacb0d3270c50793deb78e75f7f7
rikuriku <kuroe1072@gmail.com>
2019-03-08 14:44:05 +0900

    [りくりく]重力惑星の編集

--- CESA2019Project/Assets/GimmicPlanets/Gravite.cs
+++ CESA2019Project/Assets/GimmicPlanets/Gravite.cs
@@ -4,24 +4,14 @@ using UnityEngine;
 using Game.Player;
 
 
-namespace Game.Plane
+namespace Game.Planet
 {
     public class Gravite : MonoBehaviour
     {
-        const int PLAYER_LAYER = 9;
-        const int PLANET_LAYER = 10;
+       
         [SerializeField]PlayerController player;
         // Start is called before the first frame update
-        void Start()
-        {
-           
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        
         
         private void OnTriggerStay2D(Collider2D collision)
         {
