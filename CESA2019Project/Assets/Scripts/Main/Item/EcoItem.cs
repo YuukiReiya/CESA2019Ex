cc625de5ccfdc6d6791b799b9960a327ff251e19
rikuriku <kuroe1072@gmail.com>
2019-03-08 11:04:23 +0900

    [りくりく]アイテムの作成中

--- CESA2019Project/Assets/Scripts/Main/Item/EcoItem.cs
+++ CESA2019Project/Assets/Scripts/Main/Item/EcoItem.cs
@@ -17,32 +17,40 @@ public class EcoItem : Game.Item.IItem
     private void Update()
     {
         _time += Time.deltaTime;
+
+
+        if (player._jet==_ecoOxygen)
+        {
+            //5秒後に値を戻す
+            if (_time >= 5.0f)
+            {
+                func();
+            }
+
+        }
+            
+        
+        
         
     }
     protected override void GetItemSelf(PlayerController player)
     {
         //酸素消費減
-        StartCoroutine(TimeOver(player));
-        
+        //StartCoroutine(TimeOver(player));
+        player._jet = _ecoOxygen;
         Destroy(this.gameObject);
 
-        //5秒後に値を戻す
-        if (_time >= 5.0f)
-        {
-            func();
-        }
+        
     }
 
-    private IEnumerator TimeOver(PlayerController player)
-    {
-        player._jet = _ecoOxygen;
-        for(int i = 0; i < _time * 60; i++)
-        {
-            yield return null;
-        }
-        
-       
-    }
+    //private IEnumerator TimeOver(PlayerController player)
+    //{
+    //    player._jet = _ecoOxygen;
+    //    for (int i = 0; i < _time * 60; i++)
+    //    {
+    //        yield return null;
+    //    }
+    //}
     private void func()
     {
         player._jet = 10f;
